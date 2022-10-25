e38c661cfd21e0887e846577a3e2e4f11e871a78
natitomattis <natitomattis@gmail.com>
2022-10-25 07:57:28 +0000

    Added prestop to envoy sidecar (#3)

--- control-plane/connect-inject/envoy_sidecar.go
+++ control-plane/connect-inject/envoy_sidecar.go
@@ -68,6 +68,18 @@ func (w *MeshWebhook) envoySidecar(namespace corev1.Namespace, pod corev1.Pod, m
 		container.VolumeMounts = append(container.VolumeMounts, volumeMount...)
 	}
 
+	if _, ok := pod.Annotations[annotationSidecarProxyPreStopDelay]; ok {
+		preStopHook := &corev1.Lifecycle{
+			PreStop: &corev1.Handler{
+				Exec: &corev1.ExecAction{
+					Command: []string{
+						"/bin/sh",
+						"-c",
+						"sleep " + pod.Annotations[annotationSidecarProxyPreStopDelay],
+					}}}}
+		container.Lifecycle = preStopHook
+	}
+
 	tproxyEnabled, err := transparentProxyEnabled(namespace, pod, w.EnableTransparentProxy)
 	if err != nil {
 		return corev1.Container{}, err
